15b16b500a7374f8a99341ce2ff21b699aa866f1
Jacob Greenwald <jeg4t@virginia.edu>
2018-09-07 11:57:19 -0400

    add search routes with params

--- search.js
+++ search.js
@@ -3,13 +3,35 @@ const express = require('express'),
       axios = require('axios');
 
 var port = 3001;
-
+var url = 'https://images-api.nasa.gov/search?q=apollo%2011&description=moon%20landing&media_type=image',
+    url2 = 'https://images-api.nasa.gov/search';
 app.get('/', function (req, res) {
-    res.send('Hello World')
+
+
+    axios.get(url)
+      .then(function (response) {
+        res.send(response.data);
+      })
+      .catch(function (error) {
+          console.log(error);
+      });
 })
 
-app.get('/search/:query', function (req, res) {
-    res.send(req.params);
+app.get('/search/:query/:description', function (req, res) {
+    console.log(req.params.description);
+    axios.get(url2, {
+        params: {
+            q: req.params.query,
+            description: req.params.description,
+            media_type: 'image'
+        }
+    })
+      .then(function (response) {
+        res.send(response.data);
+      })
+      .catch(function (error) {
+          console.log(error);
+      });
 })
 
-app.listen(port, () => console.log(`listening on port ${port} with ${axios}! :):)`));
+app.listen(port, () => console.log(`listening on port ${port}! :):)`));
