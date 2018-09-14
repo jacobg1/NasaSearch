ae60a8c91e0250cb7f138cf00efd5400f7f4537b
Jacob Greenwald <jeg4t@virginia.edu>
2018-09-13 22:23:54 -0400

    set headers for CORS

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require('express'),
       app = express(), 
       config = require('config');
 
-var search = require('./routes/search'),
+    var search = require('./routes/search'),
     port = config.get('nasaSearch.setPort.port');
 
-app.use(search);
+// CORS middleware
+var allowCrossDomain = function (req, res, next) {
 
+    // Allowing all for now, change in prod
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+    app.use(allowCrossDomain);
+    app.use(search);
 
 app.listen(port, () => console.log(`listening on port ${port}! :):)`));
