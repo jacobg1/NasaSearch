3e25ae77806b9878129ff59096b56d995b17256e
jacobg1 <greenwald.j8@gmail.com>
2025-08-01 22:09:13 -0400

    temp lazy require mocks

--- local/mockHandlers.js
+++ local/mockHandlers.js
@@ -1,19 +1,36 @@
-const mockResponse = require("./mocks/mockResponse");
-const mockResponsePageTwo = require("./mocks/mockResponsePageTwo");
+function createPageConfig() {
+  const mockResponse = require("./mocks/mockResponse");
+  const mockResponsePageTwo = require("./mocks/mockResponsePageTwo");
 
-const pageConfig = {
-  1: mockResponse,
-  2: mockResponsePageTwo,
-};
+  const pageConfig = {
+    1: mockResponse,
+    2: mockResponsePageTwo,
+  };
+
+  return { pageConfig, defaultResponse: mockResponse };
+}
+
+function getMockResponse({ page, pageConfig, defaultResponse }) {
+  return pageConfig[page] ?? defaultResponse;
+}
 
 function createMockHandlers() {
   const { http, HttpResponse } = require("msw");
+
+  const { pageConfig, defaultResponse } = createPageConfig();
 
   const handlers = [
     http.get(process.env.API_URL, ({ request }) => {
       const { searchParams } = new URL(request.url);
       const page = searchParams.get("page");
-      return HttpResponse.json(pageConfig[page] ?? mockResponse);
+
+      return HttpResponse.json(
+        getMockResponse({
+          page,
+          pageConfig,
+          defaultResponse,
+        })
+      );
     }),
   ];
   return handlers;
