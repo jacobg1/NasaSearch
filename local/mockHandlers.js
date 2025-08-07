b934ee5d71739329debffab0c0ada3331b9177c3
jacobg1 <greenwald.j8@gmail.com>
2025-08-06 20:31:33 -0400

    add mock images to mock response

--- local/mockHandlers.js
+++ local/mockHandlers.js
@@ -1,36 +1,34 @@
+const { addMockImages } = require("./util");
+
 function createPageConfig() {
   const mockResponse = require("./mocks/mockResponse");
   const mockResponsePageTwo = require("./mocks/mockResponsePageTwo");
 
   const pageConfig = {
-    1: mockResponse,
-    2: mockResponsePageTwo,
+    1: addMockImages(mockResponse),
+    2: addMockImages(mockResponsePageTwo),
   };
 
-  return { pageConfig, defaultResponse: mockResponse };
+  return pageConfig;
 }
 
-function getMockResponse({ page, pageConfig, defaultResponse }) {
-  return pageConfig[page] ?? defaultResponse;
+function getMockResponse({ page, pageConfig }) {
+  // TODO - dynamic pages mocks?
+  const defaultPage = 1;
+  return pageConfig[page] ?? pageConfig[defaultPage];
 }
 
 function createMockHandlers() {
   const { http, HttpResponse } = require("msw");
 
-  const { pageConfig, defaultResponse } = createPageConfig();
+  const pageConfig = createPageConfig();
 
   const handlers = [
     http.get(process.env.API_URL, ({ request }) => {
       const { searchParams } = new URL(request.url);
       const page = searchParams.get("page");
 
-      return HttpResponse.json(
-        getMockResponse({
-          page,
-          pageConfig,
-          defaultResponse,
-        })
-      );
+      return HttpResponse.json(getMockResponse({ page, pageConfig }));
     }),
   ];
   return handlers;
