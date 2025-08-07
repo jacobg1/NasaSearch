214bb531e1d44f661e9e8cadd3e3826c6acc799b
jacobg1 <greenwald.j8@gmail.com>
2025-08-06 20:33:03 -0400

    re-format image with mock href

--- local/util.js
+++ local/util.js
@@ -1,3 +1,34 @@
+function createMockImageHref(items) {
+  const imageSize = process.env.IMAGE_SIZE;
+  const localImageFolder = process.env.LOCAL_IMAGE_FOLDER;
+  const localApiUrl = `http://localhost:${process.env.PORT}`;
+
+  return items.map(({ links, ...rest }) => ({
+    ...rest,
+    links: links.map((link) => {
+      if (link.href.includes(imageSize)) {
+        return {
+          ...link,
+          href: `${localApiUrl}/${localImageFolder}/${imageSize}/1.jpg`,
+        };
+      }
+      return link;
+    }),
+  }));
+}
+
+function addMockImages({ collection, ...rest }) {
+  const addMockImages = {
+    ...rest,
+    collection: {
+      ...collection,
+      items: createMockImageHref(collection.items),
+    },
+  };
+
+  return addMockImages;
+}
+
 function shouldStartLocalServer() {
   return process.env.ENV === "local" || process.env.ENV === "mocks";
 }
@@ -6,4 +37,8 @@ function shouldStartMockServer() {
   return process.env.ENV === "mocks";
 }
 
-module.exports = { shouldStartLocalServer, shouldStartMockServer };
+module.exports = {
+  shouldStartLocalServer,
+  shouldStartMockServer,
+  addMockImages,
+};
