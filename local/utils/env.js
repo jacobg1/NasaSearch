c3edbd21197661b919ff67605f274da8b1c94243
jacobg1 <greenwald.j8@gmail.com>
2025-08-08 08:16:36 -0400

    move utils to own files

---
+++ local/utils/env.js
@@ -0,0 +1,12 @@
+function shouldStartLocalServer() {
+  return process.env.ENV === "local" || process.env.ENV === "mocks";
+}
+
+function shouldStartMockServer() {
+  return process.env.ENV === "mocks";
+}
+
+module.exports = {
+  shouldStartLocalServer,
+  shouldStartMockServer,
+};
--- local/utils/mockImages.js
+++ local/utils/mockImages.js
@@ -39,16 +39,6 @@ function addMockImages({ collection, ...rest }) {
   return addMockImages;
 }
 
-function shouldStartLocalServer() {
-  return process.env.ENV === "local" || process.env.ENV === "mocks";
-}
-
-function shouldStartMockServer() {
-  return process.env.ENV === "mocks";
-}
-
 module.exports = {
-  shouldStartLocalServer,
-  shouldStartMockServer,
   addMockImages,
 };
