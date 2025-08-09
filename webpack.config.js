f06e5c3fdd8fb1803794d71c96324b1f7d602180
jacobg1 <greenwald.j8@gmail.com>
2025-08-09 09:59:02 -0400

    setup copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,36 @@
 const TerserPlugin = require("terser-webpack-plugin");
 const nodeExternals = require("webpack-node-externals");
+const CopyPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-  mode: "production",
-  devtool: false,
-  target: "node",
-  entry: ["./app.js"],
-  output: {
-    filename: "app.js",
-    libraryTarget: "commonjs2",
-  },
-  externalsPresets: { node: true },
-  externals: [nodeExternals()],
-  ignoreWarnings: [/^(?!CriticalDependenciesWarning$)/],
-  optimization: {
-    nodeEnv: false,
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-  },
+function getPlugins(mocks) {
+  if (!mocks) return [];
+
+  return [
+    new CopyPlugin({
+      patterns: [{ from: "./local/mocks/images", to: "./images" }],
+    }),
+  ];
+}
+
+module.exports = (env) => {
+  return {
+    mode: "production",
+    devtool: false,
+    target: "node",
+    entry: ["./app.js"],
+    output: {
+      filename: "app.js",
+      libraryTarget: "commonjs2",
+      clean: true,
+    },
+    externalsPresets: { node: true },
+    externals: [nodeExternals()],
+    ignoreWarnings: [/^(?!CriticalDependenciesWarning$)/],
+    optimization: {
+      nodeEnv: false,
+      minimize: true,
+      minimizer: [new TerserPlugin()],
+    },
+    plugins: getPlugins(env.mocks),
+  };
 };
