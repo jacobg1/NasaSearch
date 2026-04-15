268f4c0741a3ada3dc9dc285b333f804346e623d
jacobg1 <greenwald.j8@gmail.com>
2026-04-15 19:47:25 -0400

    remove log

--- routes/utils/request.js
+++ routes/utils/request.js
@@ -2,7 +2,6 @@ const axios = require("axios");
 const { formatResponse } = require("../utils/format");
 
 async function makeSearch(searchTerm, page) {
-  console.log("testing");
   const response = await axios.get(process.env.API_URL, {
     params: {
       q: searchTerm,
