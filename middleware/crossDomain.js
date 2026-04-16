f59af8edf55918a900a818664755d0b347b1ea9a
jacobg1 <greenwald.j8@gmail.com>
2026-04-16 14:00:44 -0400

    move cors middleware into own file

---
+++ middleware/crossDomain.js
@@ -0,0 +1,33 @@
+const { shouldStartMockServer } = require("../local/utils/env");
+
+function getAllowOrigin() {
+  const origin = process.env.WEBSITE_URL;
+
+  if (!origin || origin === "*") {
+    throw new Error("Invalid origin");
+  }
+
+  return process.env.WEBSITE_URL;
+}
+
+function getAllowMethods() {
+  return "GET";
+}
+
+function getAllowHeaders() {
+  return "Content-Type, Cache-Control, Expires, Pragma";
+}
+
+function allowCrossDomain(_, res, next) {
+  res.header("Access-Control-Allow-Origin", getAllowOrigin());
+  res.header("Access-Control-Allow-Methods", getAllowMethods());
+  res.header("Access-Control-Allow-Headers", getAllowHeaders());
+
+  if (shouldStartMockServer()) {
+    res.header("Cross-Origin-Resource-Policy", "cross-origin");
+  }
+
+  next();
+}
+
+module.exports = { allowCrossDomain };
